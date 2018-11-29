9105b15043e9e6191a13d23bfaa873ebf103dc18
ThoiDuong <dnthoi@gmail.com>
2018-11-29 22:04:38 +0700

    Adding roll function

--- app/src/main/java/com/thoi/duong/diceroller/MainActivity.kt
+++ app/src/main/java/com/thoi/duong/diceroller/MainActivity.kt
@@ -2,11 +2,34 @@ package com.thoi.duong.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnRoll: Button = findViewById(R.id.roll_button)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        btnRoll.setOnClickListener {
+            diceImage.setImageResource(rollDice())
+        }
+    }
+
+    private fun rollDice(): Int {
+        val randomInt = Random().nextInt(6)
+
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
     }
 }
